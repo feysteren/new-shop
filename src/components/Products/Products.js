6696c750b8593a4729683559c67f57d1cb4fadb8
feysteren <steren.m@gmail.com>
2021-10-25 13:32:16 +0300

    Good luck

---
+++ src/components/Products/Products.js
@@ -0,0 +1,16 @@
+import Product from "../Product/Product";
+
+function Products() {
+  return (
+    <section className="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+}
+
+export default Products;
